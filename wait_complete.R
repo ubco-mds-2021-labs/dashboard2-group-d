c8c6bf6e45fa5a69abd006c4029a7db8dc5f64c6
osbprof <rwheeler@okanagan.bc.ca>
2022-03-12 13:21:06 -0800

    wait_complete plot done, remaining date response issue

--- wait_complete.R
+++ wait_complete.R
@@ -1,3 +1,6 @@
+library(dash)
+library(dashHtmlComponents)
+library(dashCoreComponents)
 library(tidyverse)
 library(readxl)
 library(hexbin)
@@ -7,42 +10,99 @@ library(lubridate)
 library(stringr)
 library(reshape2)
 library(gridExtra)
+library(dashBootstrapComponents)
+library(ggplot2)
+library(plotly)
 
-#load data
-qdata <- read.xlsx('data/final_data_2015_2022.xlsx')
+#data manipulation
 
-# drop rows with NAs
-clean <- qdata %>% drop_na()
-count <- select(qdata, -wait_time_50, -wait_time_90)
+qdata <- read_excel('final_data_2015_2022.xlsx')
+qdata <- qdata[,-1]
 
-#cataract surgery removed
-not_cataract <- clean %>% filter(procedure != "Cataract Surgery")
+#convert <5 string to median value of 3
+qdata <- mutate_if(qdata, is.character, str_replace_all, pattern = "<5", replacement = "3")
 
-# group by health authority
-authority_data <- not_cataract %>% 
-    group_by(health_authority, year, quarter) %>% 
-    summarise(total_waiting = sum(waiting), total_completed = sum(completed)) %>% 
-    arrange(desc(total_waiting))
+clean <- qdata %>%
+  drop_na()
 
-# add ratio of completed to waitingAndcompleted
-authority_comp_prop <- authority %>%
-    mutate(ratio = total_completed/(total_completed+total_waiting))
+count <- select(clean, -wait_time_50, -wait_time_90)
 
-#prep dataframe for plotting
-melted_data <- melt(authority_data) %>% unite(time, year, quarter, sep = "")
-print(melted_data)
+main <- clean %>%
+  filter(procedure != 'All Procedures',
+         hospital != 'All Facilities',
+         health_authority != 'All Health Authorities')
+
+count <- count %>%
+  filter(procedure != 'All Procedures',
+         hospital != 'All Facilities',
+         health_authority != 'All Health Authorities')
+
+all <- clean %>%
+  filter(procedure == 'All Procedures',
+         hospital == 'All Facilities',
+         health_authority == 'All Health Authorities')
+
+no_cataract <- count %>% 
+  filter(procedure != 'Cataract Surgery')  
+
+no_cataract$waiting <- as.numeric(as.character(no_cataract$waiting))
+no_cataract$completed <- as.numeric(as.character(no_cataract$completed))
+
+hosp_list <- unique(no_cataract$hospital)
+new_list <- list()
+for(i in hosp_list){
+  sublist <- list(label = i, value = i)
+  new_list <- append(new_list, list(sublist))
+}
+
+no_cataract <- no_cataract %>% filter(health_authority==health_authority,year>=year[1],year<=year[2])
+
+hosp_data <- function(health_authority, year, hosp){
+  no_cataract <- no_cataract %>% filter(health_authority==health_authority,year>=year[1],year<=year[2])
+
+  hospital_data <- no_cataract %>% 
+      group_by(hospital, year, quarter) %>% 
+      summarise(total_waiting = sum(waiting), total_completed = sum(completed)) %>% 
+      unite(time, year, quarter, sep = "") %>%
+      arrange(desc(total_waiting))
+  melted_hosp_data <- melt(hospital_data) %>% filter(hospital == hosp)
+
+  melted_hosp_data
+
+}
+
+# wait_complete_plot <- function(health_authority, year, hosp){
+#   one_hosp_data <- hosp_data(health_authority, year, hosp)
+  
+#   wc_plot <- 
+#       ggplot(one_hosp_data, aes(x = time, y = value, fill = variable))+ 
+#           geom_bar(stat = "identity", position = 'dodge') +
+#           labs(y = "", x = 'Time', title = "Total waiting and completed cases", fill = "") +
+#           theme(text = element_text(size=16), 
+#                 axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+#                 legend.position="top")
+
+#   wc_plot
+#}
 
 app <- dash_app()
 
-#year slider
+# year slider
 yr_slider <- htmlDiv(
   list(
     dccRangeSlider(
       id="year_slider",
       min = 2015,
       max = 2022,
-      marks=list("2015" = "2015", "2016" = "2016", "2017" = "2017", "2018" = "2018",
-                 "2019" = "2019", "2020" = "2020", "2021" = "2021", "2022" = "2022"
+      marks=list(
+        "2015" = "2015",
+        "2016" = "2016",
+        "2017" = "2017",
+        "2018" = "2018",
+        "2019" = "2019",
+        "2020" = "2020",
+        "2021" = "2021",
+        "2022" = "2022"
       ),
       step=1,
       value=list(2017,2022)
@@ -67,39 +127,42 @@ ha_buttons <- htmlDiv(
 )
 
 #hospital dropdown
-# hosp_dropdown <- html.Div(
-#   list(
-#     dccDropdown(
-#       id='hospital_dropdown',
-#       options=(),
-#       value=(),
-#       clearable=False
-#     )
-#   )
-# )
+hosp_drop <- htmlDiv(
+  list(
+    dccDropdown(
+      id="hosp_drop",
+      options = new_list,
+      value = 'Kelowna General Hospital'
+    )
+  )
+)
 
-#wait_complete plot
+app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
-# wc_plot <- melted_data %>% 
-#   ggplot(hospname, aes(x = time, y = value, fill = variable)) + 
-#   geom_bar(stat = "identity", position = 'dodge') +
-#   labs(y = "Case Count",
-#       x = 'Time',
-#         title = "Total waiting and completed cases", fill = ""
-#   ) +
-#   theme(text = element_text(size=16),
-#               axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
-#               legend.position="top"
-#   )
-
-#app configuration
 app %>% set_layout(list(
   yr_slider,
   ha_buttons,
-#  hosp_dropdown
-  )
+  hosp_drop,
+  dccGraph(id = "wait_complete")
 )
-  
+)
 
+app$callback(
+  output("wait_complete", "figure"),
+  list(input("health_authority_buttons", "value"),
+       input("year_slider", "value"),
+       input("hosp_drop", "value")),
+  function(health_authority="Interior", year=c(2017,2022), hospital="Kelowna General Hospital") {
+    one_hosp_data <- hosp_data(health_authority, year, hospital)
+    wc_plot <- 
+      ggplot(one_hosp_data, aes(x = time, y = value, fill = variable))+ 
+          geom_bar(stat = "identity", position = 'dodge') +
+          labs(y = "", x = 'Time', title = "Total waiting and completed cases", fill = "") +
+          theme(text = element_text(size=16), 
+                axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+                legend.position="top")
+    ggplotly(wc_plot)
+  }
+)
 
 app$run_server(debug = T)
