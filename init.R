77a83af29884bc6d5e63e714b9c650c27b984d21
osbprof <rwheeler@okanagan.bc.ca>
2022-03-14 15:05:32 -0700

    adding packages to init file

--- init.R
+++ init.R
@@ -8,7 +8,45 @@ options(repos=r)
 
 # ======================================================================
 
+#we used these libraries, so will need to install all packages below...
+# library(dash)
+# library(dashHtmlComponents)
+# library(dashCoreComponents)
+# library(dashBootstrapComponents)
+# library(devtools)
+# library(ggplot2)
+# library(plotly)
+# library(tidyverse)
+# library(readxl)
+# library(hexbin)
+# library(ggthemes)
+# library(repr)
+# library(lubridate)
+# library(stringr)
+# library(reshape2)
+# library(gridExtra)
+# library(OpenImageR)
+
+
 # packages go here
 install.packages('remotes')
+
+install.packages('dash')
+install.packages('dashHtmlComponents')
+install.packages('dashCoreComponents')
+install.packages('dashBootstrapComponents')
+install.packages('devtools')
+install.packages('ggplot2')
+install.packages('plotly')
+install.packages('tidyverse')
+install.packages('readxl')
+install.packages('hexbin')
+install.packages('ggthemes')
+install.packages('repr')
+install.packages('lubridate')
+install.packages('stringr')
+install.packages('reshape2')
+install.packages('gridExtra')
+install.packages('OpenImageR')
 
 remotes::install_github('plotly/dashR', upgrade=TRUE)
